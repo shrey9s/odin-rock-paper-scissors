db4fd5259cdb23a024d0c4f544cc8277ce23d484
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-07 15:49:35 +1300

    Implement getComputerChoice() function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function getComputerChoice() {
     // will randomly return 'Rock', 'Paper' or 'Scissors'
+    const choices = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * 3);
+    let computerChoice = choices[randomIndex];
+    return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
