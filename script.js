0c8ec6c2b96ac55fa038e86674254bf1732003e4
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-07 14:47:43 +1300

    Add function declarations for required functions in script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    // will randomly return 'Rock', 'Paper' or 'Scissors'
+}
+
+function playRound(playerSelection, computerSelection) {
+    // plays a single round
+    // returns a string declaring winner or tie
+}
+
+function playGame() {
+    // plays a five-round game
+    // keeps score and reports winner/loser at end
+}
+
