92e0dd1dc6fbca3e2b96ee219cf06faca8e513f7
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-12 13:56:38 +1300

    Rework playGame function to keep and print the correct score between player and computer
    The function previously did not take into account ties and so stated that the player was either a winner or a loser even if they were tied with the computer.
    Also printed player and computer selections to console for greater clarity.

--- script.js
+++ script.js
@@ -9,24 +9,19 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     // plays a single round
     // returns a string declaring winner or tie
-    playerSelection = capitaliseWord(playerSelection.toLowerCase());
     if (playerSelection === computerSelection) {    // Tie
-        return `It's a tie! You both chose ${playerSelection}`;
+        return "It's a tie!";
     }
-    else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||    
-            (playerSelection === "Paper" && computerSelection === "Rock") ||
-            (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
-                return `You Win! ${playerSelection} beats ${computerSelection}`;
-            }
-    else if ((playerSelection === "Rock" && computerSelection === "Paper") || 
-            (playerSelection === "Paper" && computerSelection === "Scissors") ||
-            (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
-                return `You Lose! ${computerSelection} beats ${playerSelection}`;
-            }
-}
-
-function capitaliseWord(word) {
-    return (word.slice(0,1)).toUpperCase() + word.slice(1, word.length);
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    }
 }
 
 // Test code
@@ -38,20 +33,30 @@ function capitaliseWord(word) {
 function playGame() {
     // plays a five-round game
     // keeps score and reports winner/loser at end
-    let score = 0;
-    let playerSelection, computerSelection, result;
+    let playerScore = 0;
+    let computerScore = 0;
     for (i = 0; i < 5; i++) {
-        playerSelection = prompt("Rock, paper or scissors?");
-        computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result.includes("You Win!")) {    // Checking if player won the round
-            score += 1;
-        }
+        let playerSelection = prompt("Rock, paper or scissors?");
+        playerSelection = capitaliseWord(playerSelection.toLowerCase());
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result.includes("You Win!")) playerScore += 1;
+        else if (result.includes("You Lose!")) computerScore += 1;
+        console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.\n` + result + `\nScore: ${playerScore}:${computerScore}`);
     }
-    let finalResult = `You won ${score} round${(score >= 2) ? 's' : ''} out of 5. `;
-    finalResult += (score >= 3) ? `Congratulations, you win!` : `You lose!`;
-    console.log(finalResult);
+    if (playerScore === computerScore) {
+        console.log(`Final Score: ${playerScore}:${computerScore}\nIt's a tie!`);
+    }
+    else if (playerScore > computerScore) {
+        console.log(`Final Score: ${playerScore}:${computerScore}\nCongratulations, you win!`);
+    }
+    else if (playerScore < computerScore) {
+        console.log(`Final Score: ${playerScore}:${computerScore}\nSorry, you lose!`);
+    }
+}
+
+function capitaliseWord(word) {
+    return (word.slice(0, 1)).toUpperCase() + word.slice(1, word.length);
 }
 
 playGame();
