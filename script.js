a6b17ee57dae97ad1592af09e8c6c57f415fc249
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-12 22:11:23 +1300

    Create endGame function to stop and reset the game once player/computer reaches 5 points

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
-    
     let computerSelection = getComputerChoice();
     let result = `You chose ${playerSelection}. The computer chose ${computerSelection}. `;
     if (playerSelection === computerSelection) {    // Tie
@@ -14,35 +13,57 @@ function playRound(playerSelection) {
     }
     else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
         result += `${playerSelection} beats ${computerSelection}. You Win!`;
+        playerScore++;
     }
     else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
         result += `${computerSelection} beats ${playerSelection}. You Lose!`;
+        computerScore++;
     }
-    addResult(result);
+    displayResult(result);
+
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
+    }
 }
 
-function addResult(result) {
+function displayResult(result) {
     const resultPara = document.createElement("p");
-    const resultsDiv = document.querySelector(".results");
     resultPara.textContent = result;
+    resultPara.appendChild(document.createElement("br"));
+    resultPara.appendChild(document.createTextNode(`Score: ${playerScore}:${computerScore}`));
     resultsDiv.appendChild(resultPara);
 }
 
-//const rockBtn = document.querySelector(".rock-btn");
-//rockBtn.addEventListener("click", () => { playRound("Rock"); });
+function endGame() {
+    const finalResultPara = document.createElement("p");
+    finalResultPara.textContent = "GAME OVER!!!";
+    finalResultPara.appendChild(document.createElement("br"));
+    finalResultPara.appendChild(document.createTextNode(`Final Score: ${playerScore}:${computerScore}`));
+    finalResultPara.appendChild(document.createElement("br"));
+    finalResultPara.appendChild(document.createTextNode((playerScore === 5) ? "Congratulations, you win!" : "Sorry, you lose!"));
+    resultsDiv.appendChild(finalResultPara);
+    resultsDiv.appendChild(document.createElement("hr"));
 
-//const paperBtn = document.querySelector(".paper-btn");
-//paperBtn.addEventListener("click", () => { playRound("Paper"); });
+    // Reset game
+    playerScore = 0;
+    computerScore = 0;
+}
 
-//const scissorsBtn = document.querySelector(".scissors-btn");
-//scissorsBtn.addEventListener("click", () => { playRound("Scissors"); });
+let playerScore = 0;
+let computerScore = 0;
+const resultsDiv = document.querySelector(".results");
 
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach((btn) => btn.addEventListener("click", () => { playRound(btn.textContent); }));
 
-let playerScore = 0;
-let computerScore = 0;
-const results = document.querySelector(".results");
+
+
+// Need to keep count of score so that game stops and
+// winner is announced once a player reaches 5 points
+
+
+
+
 
 
 
