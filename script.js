9d106ceac8886d87dc1b50d83d36eaf507fcd3fd
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-12 16:23:36 +1300

    Add buttons and event listeners

--- script.js
+++ script.js
@@ -6,62 +6,36 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     // plays a single round
     // returns a string declaring winner or tie
+    let computerSelection = getComputerChoice();
+    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.\n`);
     if (playerSelection === computerSelection) {    // Tie
-        return "It's a tie!";
+        console.log("It's a tie!")
     }
     else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
-        return `${playerSelection} beats ${computerSelection}. You Win!`;
+        console.log(`${playerSelection} beats ${computerSelection}. You Win!`);
     }
     else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
         (playerSelection === "Paper" && computerSelection === "Scissors") ||
         (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
-        return `${computerSelection} beats ${playerSelection}. You Lose!`;
+        console.log(`${computerSelection} beats ${playerSelection}. You Lose!`);
     }
 }
 
-// Test code
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
+const rockBtn = document.querySelector(".rock-btn");
+rockBtn.addEventListener("click", () => { playRound("Rock"); });
 
-function playGame() {
-    // plays a five-round game
-    // keeps score and reports winner/loser at end
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i = 1; i <= 5; i++) {
-        let playerSelection = (prompt(`Round ${i}: Rock, paper or scissors?`)).toLowerCase();
-        while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-            playerSelection = (prompt("Please choose either rock, paper or scissors")).toLowerCase();
-        } // check for valid input (no empty strings or other strings/numbers)
-        playerSelection = capitaliseWord(playerSelection);
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result.includes("You Win!")) playerScore += 1;
-        else if (result.includes("You Lose!")) computerScore += 1;
-        console.log(`Round ${i}:\nYou chose ${playerSelection}. The computer chose ${computerSelection}.\n` + result + `\nScore: ${playerScore}:${computerScore}`);
-    }
-    let finalResult = `Final Score: ${playerScore}:${computerScore}\n`;
-    if (playerScore === computerScore) {
-        console.log(finalResult + "It's a tie!");
-    }
-    else if (playerScore > computerScore) {
-        console.log(finalResult + "Congratulations, you win!");
-    }
-    else if (playerScore < computerScore) {
-        console.log(finalResult + "Sorry, you lose!");
-    }
-}
+const paperBtn = document.querySelector(".paper-btn");
+paperBtn.addEventListener("click", () => { playRound("Paper"); });
 
-function capitaliseWord(word) {
-    return (word.slice(0, 1)).toUpperCase() + word.slice(1, word.length);
-}
+const scissorsBtn = document.querySelector(".scissors-btn");
+scissorsBtn.addEventListener("click", () => { playRound("Scissors"); });
 
-playGame();
+
+
+
 
