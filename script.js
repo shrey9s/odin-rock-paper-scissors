964a8b2ccfe66dfb2b97573f11a66c7ec60ecace
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-12 18:56:26 +1300

    Add div and create addResult function to display results in DOM

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function getComputerChoice() {
-    // will randomly return 'Rock', 'Paper' or 'Scissors'
     const choices = ["Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random() * 3);
     let computerChoice = choices[randomIndex];
@@ -7,34 +6,43 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
-    // plays a single round
-    // returns a string declaring winner or tie
+    
     let computerSelection = getComputerChoice();
-    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}.\n`);
+    let result = `You chose ${playerSelection}. The computer chose ${computerSelection}. `;
     if (playerSelection === computerSelection) {    // Tie
-        console.log("It's a tie!")
+        result += "It's a tie!"
     }
-    else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Paper" && computerSelection === "Rock") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
-        console.log(`${playerSelection} beats ${computerSelection}. You Win!`);
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
+        result += `${playerSelection} beats ${computerSelection}. You Win!`;
     }
-    else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
-        console.log(`${computerSelection} beats ${playerSelection}. You Lose!`);
+    else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
+        result += `${computerSelection} beats ${playerSelection}. You Lose!`;
     }
+    addResult(result);
 }
 
-const rockBtn = document.querySelector(".rock-btn");
-rockBtn.addEventListener("click", () => { playRound("Rock"); });
+function addResult(result) {
+    const resultPara = document.createElement("p");
+    const resultsDiv = document.querySelector(".results");
+    resultPara.textContent = result;
+    resultsDiv.appendChild(resultPara);
+}
 
-const paperBtn = document.querySelector(".paper-btn");
-paperBtn.addEventListener("click", () => { playRound("Paper"); });
+//const rockBtn = document.querySelector(".rock-btn");
+//rockBtn.addEventListener("click", () => { playRound("Rock"); });
 
-const scissorsBtn = document.querySelector(".scissors-btn");
-scissorsBtn.addEventListener("click", () => { playRound("Scissors"); });
+//const paperBtn = document.querySelector(".paper-btn");
+//paperBtn.addEventListener("click", () => { playRound("Paper"); });
 
+//const scissorsBtn = document.querySelector(".scissors-btn");
+//scissorsBtn.addEventListener("click", () => { playRound("Scissors"); });
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((btn) => btn.addEventListener("click", () => { playRound(btn.textContent); }));
+
+let playerScore = 0;
+let computerScore = 0;
+const results = document.querySelector(".results");
 
 
 
