e9af61ca37d52d8a61bbeafcddc36a0306ab1d79
shrey9s <145077295+shrey9s@users.noreply.github.com>
2024-03-13 15:22:08 +1300

    Modify displayResult to display only current round's result and running score Modify endGame to create text nodes instead of <p> element
    Previously, the result of each round was simply appended to the results div,
    as opposed to it overwriting the previous round's result.

--- script.js
+++ script.js
@@ -6,42 +6,41 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
+    let result;
     let computerSelection = getComputerChoice();
-    let result = `You chose ${playerSelection}. The computer chose ${computerSelection}. `;
     if (playerSelection === computerSelection) {    // Tie
-        result += "It's a tie!"
+        result = "It's a tie!"
     }
     else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {    // Player wins
-        result += `${playerSelection} beats ${computerSelection}. You Win!`;
+        result = `${playerSelection} beats ${computerSelection}. You Win!`;
         playerScore++;
     }
     else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {    // Computer wins
-        result += `${computerSelection} beats ${playerSelection}. You Lose!`;
+        result = `${computerSelection} beats ${playerSelection}. You Lose!`;
         computerScore++;
     }
-    displayResult(result);
+    displayResult(result, playerSelection, computerSelection);
 
     if (playerScore === 5 || computerScore === 5) {
         endGame();
     }
 }
 
-function displayResult(result) {
-    const resultPara = document.createElement("p");
-    resultPara.textContent = result;
-    resultPara.appendChild(document.createElement("br"));
-    resultPara.appendChild(document.createTextNode(`Score: ${playerScore}:${computerScore}`));
-    resultsDiv.appendChild(resultPara);
+function displayResult(result, playerSel, computerSel) {
+    resultsDiv.textContent = `You chose ${playerSel}. The computer chose ${computerSel}. `;
+    resultsDiv.append(document.createElement("br"));
+    resultsDiv.append(document.createTextNode(result));
+    resultsDiv.append(document.createElement("br"));
+    resultsDiv.appendChild(document.createTextNode(`Score: ${playerScore}:${computerScore}`));
 }
 
 function endGame() {
-    const finalResultPara = document.createElement("p");
-    finalResultPara.textContent = "GAME OVER!!!";
-    finalResultPara.appendChild(document.createElement("br"));
-    finalResultPara.appendChild(document.createTextNode(`Final Score: ${playerScore}:${computerScore}`));
-    finalResultPara.appendChild(document.createElement("br"));
-    finalResultPara.appendChild(document.createTextNode((playerScore === 5) ? "Congratulations, you win!" : "Sorry, you lose!"));
-    resultsDiv.appendChild(finalResultPara);
+    resultsDiv.appendChild(document.createElement("hr"));
+    resultsDiv.append(document.createTextNode("GAME OVER!!!"));
+    resultsDiv.appendChild(document.createElement("br"));
+    resultsDiv.appendChild(document.createTextNode(`Final Score: ${playerScore}:${computerScore}`));
+    resultsDiv.appendChild(document.createElement("br"));
+    resultsDiv.appendChild(document.createTextNode((playerScore === 5) ? "Congratulations, you win!" : "Sorry, you lose!"));
     resultsDiv.appendChild(document.createElement("hr"));
     // Reset game
     playerScore = 0;
